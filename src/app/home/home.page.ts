ac28e8606700907155e0068364e7dcc11d900db6
Bryan9810 <brayancan0928@hotmail.com>
2021-06-26 16:53:00 -0500

    login listo

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FacebookLoginPlugin, FacebookLogin } from '@capacitor-community/facebook-login';
+import { Platform } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,57 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  fbLogin: FacebookLoginPlugin;
+  user = null;
+  token = null;
 
+  constructor(private http: HttpClient, private platform: Platform) {
+    this.setupFbLogin();
+  }
+
+  async setupFbLogin() {
+      this.fbLogin = FacebookLogin;
+  }
+
+  async login() {
+    const FACEBOOK_PERMISSIONS = ['email', 'user_birthday'];
+    const result = await this.fbLogin.login({ permissions: FACEBOOK_PERMISSIONS });
+
+    if (result.accessToken && result.accessToken.userId) {
+      this.token = result.accessToken;
+      this.loadUserData();
+    } else if (result.accessToken && !result.accessToken.userId) {
+      // Web only gets the token but not the user ID
+      // Directly call get token to retrieve it now
+      this.getCurrentToken();
+    } else {
+      // Login failed
+    }
+  }
+
+  async getCurrentToken() {
+    const result = await this.fbLogin.getCurrentAccessToken();
+
+    if (result.accessToken) {
+      this.token = result.accessToken;
+      this.loadUserData();
+    } else {
+      // Not logged in.
+    }
+  }
+
+  async loadUserData() {
+    const url = `https://graph.facebook.com/${this.token.userId}?fields=id,name,picture.width(720),birthday,email&access_token=${this.token.token}`;
+    this.http.get(url).subscribe(res => {
+      this.user = res;
+    });
+  }
+
+  async logout() {
+    await this.fbLogin.logout();
+    this.user = null;
+    this.token = null;
+  }
+  
 }
+
